8cab9ce72a4855f9f632c7df7be24c0b1ad75371
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2015-03-04 12:38:02 -0500

    Début du trackage de la version anglaise

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-wget "http://csgames.org/2015/" -O page.html
-git add page.html
+wget "http://csgames.org/2015/" -O fr.html
+wget "http://csgames.org/2015/en" -O en.html
+
+git add {fr,en}.html
 git commit -m 'Le site a changé'
 git push origin master
