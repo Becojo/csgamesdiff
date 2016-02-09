7154a646357eb2465c00e18cb7058a15901a4481
Becojo <benoit.cote.jodoin@gmail.com>
2016-02-08 22:14:34 -0500

    track english version

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget "http://csgames.org/2016/" -O 2016.html
-wget "http://csgames.org/corpo/announcement-cs-games-2016/" -O announcement.html
+wget "http://csgames.org/2016/en/" -O 2016-en.html
 
 git add *.html
 git commit -m 'Le site a changé'
-# git push origin master
+git push origin master
