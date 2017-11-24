86223fc6b2489ef82c0844b00ba162f6acf588f5
becojo <benoit.cote.jodoin@gmail.com>
2017-11-24 16:27:04 +0000

    retrait du courriel

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L "http://2018.csgames.org/" > 2018.html
+curl -L "http://2018.csgames.org/" | sed "s/.*cf_email.*//" > 2018.html
 
 git add *.html
 git commit -m 'Le site a changé'
