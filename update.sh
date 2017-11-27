ce38ca18c9ffcfac53ee20b6d528cee43e4e0a52
raspberry pi <rpi@local.dci>
2017-11-27 18:50:32 +0000

    fix cloudflare

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -L "http://2018.csgames.org/" | sed "s/.*cf_email.*//" > 2018.html
+cd /home/pi/csgamesdiff
+
+curl -L "http://2018.csgames.org/" | sed "s/.*cf_email.*//" | sed "s/.*projects.js.*//" > 2018.html
 
 git add *.html
 git commit -m 'Le site a changé'
