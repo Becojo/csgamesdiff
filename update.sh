318cd1e49af346fbf35e2d670de123c302f91946
raspberry pi <rpi@local.dci>
2017-12-24 04:42:14 +0000

    Le site a changé

--- update.sh
+++ update.sh
@@ -2,7 +2,14 @@
 
 cd /home/pi/csgamesdiff
 
-curl -L "http://2018.csgames.org/" | sed "s/.*cf_email.*//" | sed "s/.*projects.js.*//" > 2018.html
+date >> check.log
+
+curl -L "http://2018.csgames.org/" | sed '/^\s*$/d' | sed "s/.*cf_email.*//" | sed "s/.*projects.js.*//" > 2018.html
+
+grep "could not be found" 2018.html
+if [ $? -eq 0 ]; then
+  exit
+fi
 
 git add *.html
 git commit -m 'Le site a changé'
